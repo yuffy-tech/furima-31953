cdd0eb02c0bbf0729c36d2635a7d5d9c55799d66
yuffy-tech <gakuen114@gmail.com>
2021-03-21 23:53:16 +0900

    テストコード２回目

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
-RSpec.describe User, type: :model do
-  describe "ユーザー新規登録" do
-    it "nicknameが空だと登録できない" do
+ RSpec.describe User, type: :model do
+   describe "ユーザー新規登録" do
+     it "nicknameが空だと登録できない" do
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
     end
-    it "emailが空では登録できない" do
-    end
-  end
-end
+     it "emailが空では登録できない" do
+     end
+   end
+ end

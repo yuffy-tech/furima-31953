4036f393e457e38c10b8adf56d96d2ce7697e459
yuffy-tech <gakuen114@gmail.com>
2021-03-26 18:45:43 +0900

    テストコードを正しく表示させる

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,12 @@ require 'rails_helper'
      it "nicknameが空だと登録できない" do
       user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
       user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
      it "emailが空では登録できない" do
+        user = User.new(nickname: "abe", email: "", password: "00000000", password_confirmation: "00000000")
+        user.valid?
+        expect(user.errors.full_messages).to include("Email can't be blank")
      end
    end
  end

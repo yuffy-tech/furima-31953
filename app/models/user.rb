15a8730367a17471f6088094c9ea8edaaa174fba
yuffy-tech <gakuen114@gmail.com>
2021-03-28 21:51:19 +0900

    直し⑥

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,24 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-          with_options presence: true do
-  validates :first_name
-  validates :email
-  validates :last_name
-  validates :birth_date
-  validates :nickname
-          end
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :first_name
+    validates :email
+    validates :last_name
+    validates :birth_date
+    validates :nickname
+  end
+
   validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
-  validates :first_name, format:{with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :last_name, format:{with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name_kana,format:{with: /\A[ァ-ヶー－]+\z/}
-  validates :last_name_kana,format:{with: /\A[ァ-ヶー－]+\z/}
+
+  with_options format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+   validates :first_name
+   validates :last_name
+  end
+  
+  with_options format:{with: /\A[ァ-ヶー－]+\z/} do
+   validates :first_name_kana
+   validates :last_name_kana
+  end
 end

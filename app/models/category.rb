55b89c459d7bea783561d543c14f43a12fd88d1d
yuffy-tech <gakuen114@gmail.com>
2021-04-02 19:42:29 +0900

    ActiveHashをハッシュ形式で格納

---
+++ app/models/category.rb
@@ -0,0 +1,15 @@
+class Genre < ActiveHash::Base
+    self.data = [
+        { id: 1,  name: '--' },
+        { id: 2,  name: 'レディース' },
+        { id: 3,  name: 'メンズ' },
+        { id: 4,  name: 'ベビー・キッズ' },
+        { id: 5,  name: 'インテリア・住まい・子ども' },
+        { id: 6,  name: '本・音楽・ゲーム' },
+        { id: 7,  name: 'おもちゃ・ホビー・グッズ' },
+        { id: 8,  name: '家電・スマホ・カメラ' },
+        { id: 9,  name: 'スポーツ・レジャー' },
+        { id: 10, name: 'ハンドメイト' },
+        { id: 11, name: 'その他' }
+     ]
+end

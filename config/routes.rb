017a595e58b67ff7714e499abb32e864b98d20f5
yuffy-tech <gakuen114@gmail.com>
2021-04-11 21:22:38 +0900

    showアクションのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, :furima, only: [:new, :index, :create]
+  resources :items, :furima, only: [:new, :index, :create, :show]
 end

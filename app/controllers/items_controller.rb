eaaa03369b06634bf585de3b220e5f3e3394c56e
yuffy-tech <gakuen114@gmail.com>
2021-04-14 21:01:25 +0900

    編集画面のビューを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,10 @@ class ItemsController < ApplicationController
       @item = Item.find(params[:id])
     end
 
+    def edit
+      @item= Item.find(params[:id])
+    end
+
     private
     def item_params
       params.require(:item).permit(:name, :image, :text, :category_id, :status_id, :delivery_burden_id, :start_area_id, :delivery_day_id, :price).merge(user_id: current_user.id)

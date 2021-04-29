e36c1c3189f24b64f21173ec438d314c480c20d9
yuffy-tech <gakuen114@gmail.com>
2021-04-29 17:03:30 +0900

    購入のためのフォームを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
-  # root to: 'orders#index'
-  # resources :orders, only:[:create]
+  resources :items do
+    resources :orders, only: :create
+  end
 end
